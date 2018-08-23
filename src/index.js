37e2ec7e934d85c8d860aa3244a256caaa775813
yaw asabere <yasabere@gmail.com>
2018-08-22 23:12:09 -0400

    added maps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+ScrollMagic = require("scrollmagic");
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 33.753746, lng: -84.38633 },
+    zoom: 16
   });
 }
+
+let controller = new ScrollMagic.Controller({
+  globalSceneOptions: {
+    triggerHook: "onEnter",
+    duration: "200%"
+  }
+});
